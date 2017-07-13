f8042b10d6f3bdc9fbc6d3df31268e51aabecd57
Cesar <cesarrobledo1990@gmail.com>
2017-07-13 09:00:18 -0300

    refs #18 agregado borrado de alquileres

--- deptos/departamentos/tests.py
+++ deptos/departamentos/tests.py
@@ -80,3 +80,99 @@ class DepartamentosCrearTests(TestCase):
             'usuario': usuariotest,
         })
         self.assertTrue("localidad" in response.context["form"].errors)
+
+class DepartamentosActualizarTests(TestCase):
+
+    def test_editarAlquiler_templates_used(self):
+        #Logueamos y creamos un usuario default...
+        user = User.objects.create(username='testuser')
+        user.set_password('1234')
+        user.save()
+        self.client.login(username='testuser', password='12345')
+        usuariotest = Usuario.objects.create(telefono="333333", direccion="dir_test", usuario=user)
+
+        #Creamos un alquiler...
+        alquilertest = Departamento.objects.create(titulo='titulotest', descripción='asdtest', latitud=11.111, longitud=22.2222, capacidad=4, localidad='localidadtest', precio=10000,usuario=usuariotest)
+
+        # testea que usamos el template 'departamentos/departamento_form.html' al editar...
+        response = self.client.get(reverse('departamentos:alquiler_editar', args=(alquilertest.pk,)))
+        self.assertTemplateUsed(response, 'departamentos/departamento_form.html')
+
+    def test_editarAlquiler_context_view(self):
+        #Logueamos y creamos un usuario default...
+        user = User.objects.create(username='testuser')
+        user.set_password('1234')
+        user.save()
+        self.client.login(username='testuser', password='12345')
+        usuariotest = Usuario.objects.create(telefono="333333", direccion="dir_test", usuario=user)
+
+        #Creamos un alquiler por default...
+        alquilertest = Departamento.objects.create(titulo='titulotest', descripción='asdtest', latitud=11.111, longitud=22.2222, capacidad=4, localidad='localidadtest', precio=10000,usuario=usuariotest)
+
+        # datos en context de la vista
+        # por get, llega el form.
+        response = self.client.get(reverse('departamentos:alquiler_editar', args=(alquilertest.pk,)))
+        self.assertTrue("form" in response.context)
+        self.assertTrue(isinstance(response.context["form"], DepartamentoForm))
+
+    def test_editarAlquiler_post(self):
+        #Logueamos y creamos un usuario default...
+        user = User.objects.create(username='testuser')
+        user.set_password('1234')
+        user.save()
+        self.client.login(username='testuser', password='12345')
+        usuariotest = Usuario.objects.create(telefono="333333", direccion="dir_test", usuario=user)
+
+        #Creamos un alquiler por default...
+        alquilertest = Departamento.objects.create(titulo='titulotest', descripción='asdtest', latitud=11.111, longitud=22.2222, capacidad=4, localidad='localidadtest', precio=10000,usuario=usuariotest)
+
+        #Actualizamos datos y obtenemos respuesta exitosa
+        response = self.client.post(reverse('departamentos:alquiler_editar', args=(alquilertest.pk,)), {
+            'titulo': 'new titulotest',
+			'descripción': 'new asdtest',
+			'latitud': 12.123,
+			'longitud': 23.456,
+			'capacidad': 3,
+            'localidad': 'new localidadtest',
+            'precio': 5000,
+            'usuario': usuariotest,
+        })
+        self.assertEqual(response.status_code, 302)
+        #Comprobamos la actualizacion de los datos...
+        self.assertEqual(Departamento.objects.filter(titulo="new titulotest").count(), 1)
+        self.assertTrue(Departamento.objects.filter(precio=5000).exists())
+
+    def test_borrarAlquiler_templates_used(self):
+        #Logueamos y creamos un usuario default...
+        user = User.objects.create(username='testuser')
+        user.set_password('1234')
+        user.save()
+        self.client.login(username='testuser', password='12345')
+        usuariotest = Usuario.objects.create(telefono="333333", direccion="dir_test", usuario=user)
+
+        #Creamos un alquiler por default...
+        alquilertest = Departamento.objects.create(titulo='titulotest', descripción='asdtest', latitud=11.111, longitud=22.2222, capacidad=4, localidad='localidadtest', precio=10000,usuario=usuariotest)
+
+        # testea que usamos el template 'departamentos/departamento_form.html' al editar...
+        response = self.client.get(reverse('departamentos:alquiler_borrar', args=(alquilertest.pk,)))
+        self.assertTemplateUsed(response, 'departamentos/borrado_alquiler.html')
+
+    def test_borrarAlquiler(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('12345')
+        user.save()
+        self.client.login(username='testuser', password='12345')
+        usuariotest = Usuario.objects.create(telefono="333333", direccion="dir_test", usuario=user)
+        # Creo depto nº1..
+        Departamento.objects.create(titulo="titulo1", descripción="descrip1", latitud=10.000, longitud=10.000,capacidad=1,precio=1000,usuario=usuariotest)
+        # Creo depto nº2..
+        Departamento.objects.create(titulo="titulo2", descripción="descrip2", latitud=20.000, longitud=20.000,capacidad=2,precio=2000,usuario=usuariotest)
+        resp = self.client.get(reverse('departamentos:home'))
+        # Debe identificar 2 alquileres creados en el listado...
+        self.assertEqual(len(resp.context["alquileres"]), 2)
+        # Borramos uno y deberá actualizarse a 1 la cantidad de alquileres...
+        depto = Departamento.objects.get(pk=1)
+        depto.delete();
+        resp = self.client.get(reverse('departamentos:home'))
+        self.assertEqual(len(resp.context["alquileres"]), 1)
+        self.assertTrue(Departamento.objects.filter(titulo='titulo2').exists())
--- deptos/departamentos/urls.py
+++ deptos/departamentos/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
-from .views import alquiler_nuevo, home
+from .views import alquiler_nuevo, home, alquiler_lista, alquiler_editar, alquiler_borrar
 from django.contrib.auth.decorators import login_required
 
 urlpatterns = [
     url(r'^$', "departamentos.views.home", name='home'),
     url(r'^nuevoAlquiler$', "departamentos.views.alquiler_nuevo", name='alquiler_crear'),
+    url(r'^listadoAlquileres$', "departamentos.views.alquiler_lista", name='alquiler_listado'),
+    url(r'^editarArquiler/(?P<id_alquiler>\d+)/$', "departamentos.views.alquiler_editar", name='alquiler_editar'),
+    url(r'^eliminarArquiler/(?P<id_alquiler>\d+)/$', "departamentos.views.alquiler_borrar", name='alquiler_borrar'),
 ]
