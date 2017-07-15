c59639b9aa4d133fbfc7477b87be40c1f54e3acf
Cesar <cesarrobledo1990@gmail.com>
2017-07-14 22:37:22 -0300

    refs #18 agregado la baja y editado de alquileres

--- deptos/deptos/urls.py
+++ deptos/deptos/urls.py
@@ -4,7 +4,12 @@ from django.contrib.auth.views import login, logout
 
 urlpatterns = [
     url(r'^$', "departamentos.views.home", name='home'),
+    ##ABM ALQUILERES##
     url(r'^nuevoAlquiler$', "departamentos.views.alquiler_nuevo", name='alquiler_crear'),
+    url(r'^listadoAlquileres$', "departamentos.views.alquiler_lista", name='alquiler_listado'),
+    url(r'^editarArquiler/(?P<id_alquiler>\d+)/$', "departamentos.views.alquiler_editar", name='alquiler_editar'),
+    url(r'^eliminarArquiler/(?P<id_alquiler>\d+)/$', "departamentos.views.alquiler_borrar", name='alquiler_borrar'),
+    #####FIN-ABM######
     url(r'^admin/', include(admin.site.urls)),
     url(r'^registrar/$', 'usuarios.views.signup', name='signup'),
     url(r'^ingresar/$', login, {'template_name': 'usuarios/login.html', }, name="login"),
